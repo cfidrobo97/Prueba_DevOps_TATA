520543494af4a948239e93e3928bab84035dd566
cfidrobo97 <cristianidrobo97@gmail.com>
2025-11-19 23:05:02 -0500

    test(ci): pruebas básicas + Jacoco/SpotBugs configurados en Maven

---
+++ src/test/java/com/devops/devops_service/DevopsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devops_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
